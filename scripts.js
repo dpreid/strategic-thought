45392041939052c48d1c1e133ef8c95fddd67b75
dpreid <30912999+dpreid@users.noreply.github.com>
2018-03-10 12:56:30 +0900

    Test
    Testing lack of main page text

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mysidemenu").style.width = "250px";
+    document.getElementById("mainpage").style.marginLeft = "250px";
+    document.getElementById("pagehead").style.marginLeft = "250px";
+
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mysidemenu").style.width = "0px";
+    document.getElementById("mainpage").style.marginLeft = "0px";
+    document.getElementById("pagehead").style.marginLeft = "0px";
 }
