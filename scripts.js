8afc1fc2692b4253fd805c1ff36e2fe826cdaf5b
dpreid <30912999+dpreid@users.noreply.github.com>
2018-07-22 22:45:40 +0100

    opticslab tutorial update

--- scripts.js
+++ scripts.js
@@ -12,3 +12,19 @@ function closeNav() {
     document.getElementById("mainpage").style.marginLeft = "0px";
     document.getElementById("pagehead").style.marginLeft = "0px";
 }
+
+/* Reveal images that are hidden */
+/* Bad solution, but simply adding all the images I want hidden to this function */
+function revealImage(){
+  document.getElementById("hiddenImage1").style.opacity=1.0;
+  document.getElementById("hiddenImage2").style.opacity=1.0;
+  
+
+}
+
+/* Hide image again */
+function hideImage(){
+  //console.log("Running");
+  document.getElementById("hiddenImage1").style.opacity=0.0;
+  document.getElementById("hiddenImage2").style.opacity=0.0;
+}
