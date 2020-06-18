c4f65c8a5bbb956b9e0e4bdb83e6beb494bf8840
dpreid <30912999+dpreid@users.noreply.github.com>
2020-06-18 13:21:39 +0900

    Update homepage

--- UnityWebAppTest/unity_test_javascript_functions.js
+++
@@ -1,8 +0,0 @@
-
-
-function AddClick() {
-    console.log('val = ' + document.getElementById('click_field').value);
-    var current_val = document.getElementById('click_field').value;
-    document.getElementById('click_field').value = Number(current_val) + 1;
-
-}
